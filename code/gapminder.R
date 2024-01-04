ba52282d90442363082e589bf255dae976598052
Kwangchun Lee <kwangchun.lee.7@gmail.com>
2024-01-04 18:57:15 +0900

    start animation

---
+++ code/gapminder.R
@@ -0,0 +1,84 @@
+library(tidyverse)
+library(gganimate)
+library(gghighlight)
+library(extrafont)
+extrafont::loadfonts()
+library(gapminder)
+library(gifski)
+
+# 1. gapminder ------------------------------------------------------------
+
+p <- ggplot(
+  gapminder, 
+  aes(x = gdpPercap, y=lifeExp, size = pop, colour = country)
+) +
+  geom_point(show.legend = FALSE, alpha = 0.7) +
+  scale_color_viridis_d() +
+  scale_size(range = c(2, 12)) +
+  scale_x_log10() +
+  labs(x = "GDP per capita", y = "Life expectancy")
+p
+
+gapminder_gif <- p + transition_time(year) +
+  labs(title = "Year: {frame_time}")
+
+anim_save(filename = "images/gapminder.gif")
+
+
+# 2. airquality -----------------------------------------------------------
+
+mean.temp <- airquality %>%
+  group_by(Month) %>%
+  summarise(Temp = mean(Temp))
+mean.temp
+
+p <- ggplot(mean.temp, aes(Month, Temp, fill = Temp)) +
+  geom_col() +
+  scale_fill_distiller(palette = "Reds", direction = 1) +
+  theme_minimal() +
+  theme(
+    panel.grid = element_blank(),
+    panel.grid.major.y = element_line(color = "white"),
+    panel.ontop = TRUE
+  )
+p
+
+p + transition_states(Month, wrap = FALSE) +
+  shadow_mark()
+
+anim_save(filename = "images/airquality.gif", device = 'ragg_png')
+
+
+# 3. high quality ---------------------------------------------------------
+
+# 3.1. 좋지 않음 --------------------------------------------------------------
+
+gplot <- 
+  ggplot(
+    gapminder,
+    aes(x = gdpPercap, y = lifeExp, colour = continent, size = pop)
+  ) +
+  geom_point(alpha = 0.6) +
+  scale_x_log10() +
+  transition_time(year)
+
+magick::image_write(
+  anim_save("images/high_quality.gif", gplot, width = 1000, height = 1000)
+)
+
+
+# 3.2. iris ---------------------------------------------------------------
+
+p <- ggplot(iris, aes(x = Petal.Width, y = Petal.Length)) +
+  geom_point()
+
+anim <- p +
+  transition_states(Species,
+                    transition_length = 2,
+                    state_length = 1)
+
+iris_ani <- animate(anim, duration = 3.3, 
+                    fps = 10, width = 1400, height = 865, 
+                    renderer = gifski_renderer(), res = 200, type = "cairo")
+
+anim_save("images/iris.gif", animation = iris_ani)
---
+++ code/자살.R
@@ -0,0 +1,87 @@
+library(tidyverse)
+library(gganimate)
+library(gghighlight)
+library(extrafont)
+extrafont::loadfonts()
+
+# 1. 데이터 출처 --------------------------------------------------------------------------
+## https://data.oecd.org/healthstat/suicide-rates.htm
+
+suicide_dat <- read_csv("data/DP_LIVE_04012024092405729.csv", col_names=TRUE)
+
+# korea_friends <- c("Korea", "Germany", "Spain", "Japan", "United States", "OECD countries")
+korea_friends <- c("한국", "독일", "스페인", "일본", "미국", "프랑스")
+# korea_friends <- c("KOR", "DEU", "ESP", "JPN", "USA", "FRA")
+
+
+# 2. 데이터 정제 --------------------------------------------------------------------------
+suicide_dat$LOCATION <- plyr::revalue(suicide_dat$LOCATION, c("DEU" ="독일",
+                                                              "KOR" = "한국",
+                                                              "ESP" = "스페인",
+                                                              "USA" = "미국",
+                                                              "JPN" = "일본",
+                                                              "FRA" = "프랑스"))
+
+suicide_df <- suicide_dat %>% dplyr::filter(LOCATION %in% korea_friends & SUBJECT =="TOT") %>% 
+  dplyr::select(country=LOCATION, TIME, suicide=Value) %>% 
+  mutate(date = ymd(paste0(TIME,"-01-01"))) %>% dplyr::select(-TIME)
+
+dlist <- unique(suicide_df$date)
+
+# 3. 십만명당 자살자수 시각화  --------------------------------------------------------------------------
+## 3.1. 십만명당 자살자수 국제 비교 ------------------------------------------------------------------
+
+ggplot(data=suicide_df,
+       aes(x=date, y=suicide, group=country, color=country, label=country))+
+  geom_line(linewidth=1.1) +
+  scale_x_date(breaks=seq(dlist[1], tail(dlist,1) + years(1), "5 year"),
+               date_labels="%y",limits=c(dlist[1], tail(dlist,1)+years(1)))+
+  facet_wrap(~country)+
+  theme_korean() +
+  theme(legend.position="none", plot.caption=element_text(hjust=0,size=8),plot.subtitle=element_text(face="italic"),
+        axis.text=element_text(size=7.5))+
+  labs(x="",y="",title="자살자수 국제 비교",
+       caption="\n 자료출처: 십만명당 자살자수 OECD 데이터, https://data.oecd.org/healthstat/suicide-rates.htm",
+       subtitle="십만명당 자살자수(남녀 총합)") 
+
+## 3.2. 한국 등 일괄비교 ---------------------------------------------
+
+suicide_df |> 
+  ggplot(aes(x = date, y = suicide, group=country, color=country, label=country))+
+    geom_line(linewidth = 0.7) +
+    geom_point(size = 1.0) +
+    scale_x_date(breaks=seq(dlist[1], tail(dlist,1) + years(1), "5 year"),
+                 date_labels="%y",limits=c(dlist[1], tail(dlist,1)+years(5)))+
+    geom_text(data=suicide_df %>% dplyr::filter(date==dlist[54]), hjust=0, nudge_x=500) +
+    theme_korean() +
+    labs(x="",y="",title="자살자수 국제 비교",
+         caption="\n 자료출처: 십만명당 자살자수 OECD 데이터, https://data.oecd.org/healthstat/suicide-rates.htm",
+         subtitle="십만명당 자살자수(남녀 총합)") 
+
+## 3.3. 애니메이션 ---------------------------------------------
+extrafont::loadfonts()
+# https://stackoverflow.com/questions/64762634/r-animating-line-plot-using-gganimate-and-geom-text
+
+suicide_gif <- suicide_df |> 
+  filter(date >= ymd("2010-01-01")) |> 
+  ggplot(aes(x=date, y=suicide, group = country, color = country, label=country))+
+    geom_point(show.legend = FALSE) +
+    geom_line(linewidth = 0.7, aes(group = country)) +
+    scale_color_viridis_d() +
+    scale_x_date(limits = as.Date(c("2010-01-01", "2030-12-31")),
+                 date_labels="%y") +
+    geom_text(aes(x = date, label = country), 
+              nudge_x = -1, hjust = -0.4, show.legend=FALSE, size = 3.5) +  
+    theme_korean() +
+    theme(legend.position = "none")+
+    labs(x="",
+         y="",
+         title="자살자수 국제 비교",
+         caption="\n 자료출처: 십만명당 자살자수 OECD 데이터, https://data.oecd.org/healthstat/suicide-rates.htm",
+         subtitle="십만명당 자살자수(남녀 총합)")  +
+  transition_reveal(date, keep_last=TRUE)   
+
+
+anim_save(suicide_gif, path = "images/suicide.gif")
+
+
